f2058f1c634b3cc5f3d2d9b6260ed5dd714da7cc
jimmdd <jimdingmac@gmail.com>
2025-05-11 22:49:28 -0700

    fix the minting account instruction

--- programs/zesty/src/instructions/mint_compressed_tokens.rs
+++ programs/zesty/src/instructions/mint_compressed_tokens.rs
@@ -1,7 +1,17 @@
+use account_compression::program::AccountCompression;
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{
+    instruction::{AccountMeta, Instruction},
+    program::invoke_signed,
+};
+use anchor_lang::AnchorSerialize;
+
 use anchor_spl::token_2022::Token2022;
-use anchor_spl::token_interface::{Mint, TokenInterface};
+use anchor_spl::token_interface::Mint;
+// use light_compressed_token::cpi::mint_to;
+// use light_compressed_token::process_transfer::get_cpi_authority_pda;
 use light_compressed_token::program::LightCompressedToken;
+use light_system_program::program::LightSystemProgram;
 
 #[derive(Accounts)]
 pub struct EmitEvent {}
@@ -10,9 +20,10 @@ pub struct EmitEvent {}
 pub struct CustomEvent {
     pub message: String,
 }
-// // Mint zk-compressed tokens to a customer based on Solana Pay transfer amount
+
+// Mint zk-compressed tokens to a customer based on Solana Pay transfer amount
 pub fn mint_compressed_tokens(
-    _ctx: Context<MintCompressedTokens>,
+    ctx: Context<MintCompressedTokens>,
     transfer_amount: u64, // Lamports (SOL * 10^9)
     token_rate: u64,      // Tokens per lamport (e.g., 10000 = 100 tokens per 0.01 SOL)
 ) -> Result<()> {
@@ -25,23 +36,122 @@ pub fn mint_compressed_tokens(
         message: tokens_to_mint.to_string()
     });
 
+    let recipient = ctx.accounts.recipient.key();
+    let amounts = vec![tokens_to_mint];
+    let recipients = vec![recipient];
+
+    let signer_seeds = &[
+        b"mint_auth",
+        ctx.accounts.mint_creator.key.as_ref(),
+        &[ctx.bumps.mint_authority],
+    ];
+    let expected_pda = Pubkey::create_program_address(
+        &[
+            b"mint_auth",
+            ctx.accounts.mint_creator.key.as_ref(),
+            &[ctx.bumps.mint_authority],
+        ],
+        &crate::ID,
+    )
+    .unwrap();
+
+    msg!("Expected PDA: {}", expected_pda);
+    msg!("Provided PDA: {}", ctx.accounts.mint_authority.key());
+    // manual cpi call
+    let mut data: Vec<u8> = vec![241, 34, 48, 186, 37, 179, 123, 192];
+    data.extend(recipients.try_to_vec()?);
+    data.extend(amounts.try_to_vec()?);
+    data.extend(Option::<u64>::None.try_to_vec()?); // no lamports
+    let accounts = light_compressed_token::accounts::MintToInstruction {
+        fee_payer: ctx.accounts.recipient.key(),
+        authority: ctx.accounts.mint_authority.key(),
+        cpi_authority_pda: ctx.accounts.compressed_token_cpi_authority_pda.key(),
+        mint: ctx.accounts.mint.key(),
+        token_pool_pda: ctx.accounts.token_pool_pda.key(),
+        token_program: ctx.accounts.token_program.key(),
+        light_system_program: ctx.accounts.light_system_program.key(),
+        registered_program_pda: ctx.accounts.registered_program_pda.key(),
+        noop_program: ctx.accounts.noop_program.key(),
+        account_compression_authority: ctx.accounts.account_compression_authority_pda.key(),
+        account_compression_program: ctx.accounts.account_compression_program.key(),
+        merkle_tree: ctx.accounts.state_tree.key(),
+        self_program: ctx.accounts.compressed_token_program.key(),
+        system_program: ctx.accounts.system_program.key(),
+        sol_pool_pda: None,
+    };
+    let ix = Instruction {
+        program_id: light_compressed_token::ID,
+        accounts: accounts.to_account_metas(Some(true)),
+        data: data,
+    };
+
+    invoke_signed(
+        &ix,
+        &[
+            ctx.accounts.recipient.to_account_info(),
+            ctx.accounts.mint_authority.to_account_info(),
+            ctx.accounts
+                .compressed_token_cpi_authority_pda
+                .to_account_info(),
+            ctx.accounts.mint.to_account_info(),
+            ctx.accounts.token_pool_pda.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.light_system_program.to_account_info(),
+            ctx.accounts.registered_program_pda.to_account_info(),
+            ctx.accounts.noop_program.to_account_info(),
+            ctx.accounts
+                .account_compression_authority_pda
+                .to_account_info(),
+            ctx.accounts.account_compression_program.to_account_info(),
+            ctx.accounts.state_tree.to_account_info(),
+            ctx.accounts.compressed_token_program.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+        ],
+        &[signer_seeds],
+    )?;
+
     Ok(())
 }
 
 #[derive(Accounts)]
 #[instruction(transfer_amount: u64, token_rate: u64)]
 pub struct MintCompressedTokens<'info> {
-    mint: InterfaceAccount<'info, Mint>,
+    #[account(mut)]
+    pub mint: InterfaceAccount<'info, Mint>,
     /// CHECK: Validated by compressed token program
-    // pub compressed_pda: AccountInfo<'info>,
+    #[account(mut)]
+    pub token_pool_pda: AccountInfo<'info>,
     /// CHECK: Validated by compressed token program
+    #[account(mut)]
+    pub compressed_token_cpi_authority_pda: AccountInfo<'info>,
+    #[account(mut)]
+    /// CHECK: Validated by compressed token program
+    pub account_compression_authority_pda: AccountInfo<'info>,
+    #[account(mut)]
+    /// CHECK: Validated by compressed token program
+    pub registered_program_pda: AccountInfo<'info>,
+    /// CHECK: Validated by compressed token program
+    pub noop_program: AccountInfo<'info>,
+    /// CHECK: only use for derive mint auth pda
+    pub mint_creator: AccountInfo<'info>,
+    #[account(
+        mut,
+        seeds = [b"mint_auth", mint_creator.key().as_ref()],
+        bump
+    )]
+    pub mint_authority: AccountInfo<'info>,
     #[account(mut)]
     pub state_tree: AccountInfo<'info>,
     #[account(mut)]
     pub recipient: Signer<'info>,
-    pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token2022>,
+    pub system_program: Program<'info, System>,
+    pub light_system_program: Program<'info, LightSystemProgram>,
+    pub account_compression_program: Program<'info, AccountCompression>,
     pub compressed_token_program: Program<'info, LightCompressedToken>,
+    /// CHECK: Validated by compressed token program
+    #[account(mut)]
+    pub sol_pool_pda: Option<AccountInfo<'info>>,
 }
 
 #[error_code]
@@ -55,3 +165,35 @@ pub enum ZestyMintError {
     #[msg["Not enough balance in burner account"]]
     NotEnoughBalance,
 }
+
+// // finding the light accounts
+// let registered_program_pda =
+//     Pubkey::find_program_address(&[PROGRAM_ID_LIGHT_SYSTEM.as_ref()], &crate::ID).0;
+
+// let self_program_cpi_authority_pda = light_sdk::utils::get_cpi_authority_pda(&crate::ID);
+
+// let cpi_ctx = CpiContext::new_with_signer(
+//     ctx.accounts.compressed_token_program.to_account_info(),
+//     MintToInstruction {
+//         fee_payer: ctx.accounts.recipient.to_account_info(),
+//         authority: ctx.accounts.mint_authority.to_account_info(),
+//         mint: ctx.accounts.mint.to_account_info(),
+//         token_pool_pda: ctx.accounts.token_pool_pda.to_account_info(),
+//         token_program: ctx.accounts.token_program.to_account_info(),
+//         merkle_tree: ctx.accounts.state_tree.to_account_info(),
+//         system_program: ctx.accounts.system_program.to_account_info(),
+//         account_compression_authority: ctx.accounts.mint_authority.to_account_info(),
+//         // derived accounts
+//         cpi_authority_pda: ctx
+//             .accounts
+//             .compressed_token_cpi_authority_pda
+//             .to_account_info(),
+//         light_system_program: ctx.accounts.light_system_program.to_account_info(),
+//         registered_program_pda: ctx.accounts.registered_program_pda.to_account_info(),
+//         noop_program: ctx.accounts.noop_program.to_account_info(),
+//         account_compression_program: ctx.accounts.account_compression_program.to_account_info(),
+//         self_program: ctx.accounts.compressed_token_program.to_account_info(),
+//         sol_pool_pda: None,
+//     },
+//     signer,
+// );
--- tests/zesty.ts
+++ tests/zesty.ts
@@ -1,21 +1,33 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { getMinimumBalanceForRentExemptMint, getMint, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import { createInitializeMint2Instruction, createInitializeMintInstruction, getMinimumBalanceForRentExemptMint, getMint, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
 import {
    LightSystemProgram,
    Rpc,
+   accountCompressionProgram,
    confirmTx,
    createRpc,
+   getAccountCompressionAuthority,
+   getRegisteredProgramPda,
+   noopProgram,
 } from "@lightprotocol/stateless.js";
-import { createMint, mintTo, transfer } from "@lightprotocol/compressed-token";
-import { Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js";
+import { approveAndMintTo, createMint, mintTo, transfer, CompressedTokenProgram, createTokenPool, getTokenPoolInfos, CPI_AUTHORITY_SEED } from "@lightprotocol/compressed-token";
+import { Connection, Keypair, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 import { Zesty } from "../target/types/zesty";
+import { getCompressionMintIx } from "./lightCompressionProgram";
+import { min } from "bn.js";
+import { SPL_NOOP_PROGRAM_TAG } from "@lightprotocol/zk-compression-cli";
+import { assert } from "chai";
 
 
 describe("mint compressed token", () => {
-   const payer = Keypair.generate();
-   const tokenRecipient = Keypair.generate();
-   const lightCompressedTokenProgram = new PublicKey('cTokenmWW8bLPjZEBAUgYy3zKxQZW6VKi7bqNFEVv3m')
+   // use fixed keypairs for testing 
+   const payer = Keypair.fromSecretKey(Uint8Array.from([47, 190, 85, 201, 226, 79, 55, 28, 180, 94, 99, 30, 188, 149, 1, 242, 27, 248, 119, 43, 164, 204, 142, 19, 93, 112, 30, 100, 65, 112, 78, 51, 245, 89, 172, 170, 173, 163, 188, 63, 93, 35, 79, 252, 94, 183, 78, 17, 156, 136, 89, 32, 70, 57, 124, 15, 130, 161, 23, 103, 60, 68, 179, 207]));
+   console.log("Payer public key", payer.publicKey.toBase58());
+   const tokenRecipient = Keypair.fromSecretKey(Uint8Array.from([173, 197, 50, 131, 39, 29, 39, 250, 159, 196, 73, 134, 58, 29, 223, 8, 28, 2, 99, 49, 210, 52, 247, 89, 99, 21, 50, 36, 230, 87, 154, 166, 178, 174, 70, 242, 176, 126, 88, 41, 159, 168, 242, 142, 158, 98, 184, 241, 75, 202, 115, 21, 146, 148, 43, 49, 14, 82, 252, 71, 158, 41, 54, 89]));
+   console.log("Recipient public key", tokenRecipient.publicKey.toBase58());
+   const mint = Keypair.fromSecretKey(Uint8Array.from([202, 211, 31, 147, 122, 247, 234, 6, 79, 147, 27, 72, 49, 8, 137, 128, 250, 27, 51, 16, 187, 218, 50, 164, 183, 232, 34, 130, 139, 1, 116, 94, 75, 181, 213, 91, 43, 5, 77, 50, 214, 89, 19, 83, 114, 142, 143, 240, 253, 174, 22, 3, 157, 147, 40, 224, 177, 24, 171, 197, 197, 102, 244, 137]));
+   console.log("Mint public key", mint.publicKey.toBase58());
    // Configure the client to use the local cluster.
    const provider = new anchor.AnchorProvider(
       new Connection("http://localhost:8899"),
@@ -24,25 +36,24 @@ describe("mint compressed token", () => {
    );
    anchor.setProvider(provider);
    const program = anchor.workspace.Zesty as Program<Zesty>;
-   const connection2: Rpc = createRpc();
-   const mint = anchor.web3.Keypair.generate();
+   const connection: Rpc = createRpc();
    // find the mint authority PDA
    const [mintAuthorityPda, bump] = PublicKey.findProgramAddressSync(
       [Buffer.from("mint_auth"), payer.publicKey.toBuffer()],
       program.programId
    );
 
-   it("Test create new mint with pda auth", async () => {
+   it("create new mint with pda auth", async () => {
       // Airdrop SOL to payer
       const sig = await confirmTx(
-         connection2,
-         await connection2.requestAirdrop(payer.publicKey, 10e9)
+         connection,
+         await connection.requestAirdrop(payer.publicKey, 10e9)
       );
       console.log("Payer Airdrop confirmed", sig);
       console.log("Payer Airdrop done");
 
       // get the minimum balance for rent exempt mint
-      const lamports = await getMinimumBalanceForRentExemptMint(connection2);
+      const lamports = await getMinimumBalanceForRentExemptMint(connection);
 
       console.log("mint authority PDA", mintAuthorityPda.toBase58());
       console.log("mint setup done")
@@ -69,46 +80,86 @@ describe("mint compressed token", () => {
          ]).instruction();
       const transaction = new Transaction().add(tx);
       console.log("transaction", transaction);
-      await confirmTx(connection2, await connection2.sendTransaction(transaction, [payer, mint], { skipPreflight: false, preflightCommitment: "confirmed" }));
+      await confirmTx(connection, await connection.sendTransaction(transaction, [payer, mint], { skipPreflight: false, preflightCommitment: "confirmed" }));
       console.log("Your transaction signature", tx);
       console.log("Mint created:", mint.publicKey.toBase58());
       console.log("Mint authority (PDA):", mintAuthorityPda.toBase58());
-   })
 
-   it("Test compressed token mint", async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const sig = await confirmTx(
-         connection2,
-         await connection2.requestAirdrop(tokenRecipient.publicKey, 10e9)
-      );
-      console.log("Airdrop confirmed", sig);
-      console.log("Airdrop done");
-      const stateTreeInfos = await connection2.getStateTreeInfos();
-      console.log("state tree infos", stateTreeInfos);
-      const transferAmount = 1e9;
-      const tokenRate = 1000;
-      const tx = await program.methods.mintCompressedTokens(new anchor.BN(transferAmount), new anchor.BN(tokenRate))
-         .accounts({
-            mint: mint.publicKey,
-            stateTree: stateTreeInfos[0].tree,
-            recipient: tokenRecipient.publicKey,
-            systemProgram: anchor.web3.SystemProgram.programId,
-            tokenProgram: TOKEN_2022_PROGRAM_ID,
-            compressedTokenProgram: lightCompressedTokenProgram,
-         }).instruction();
+      const mintInfo = await getMint(connection, mint.publicKey, undefined, TOKEN_2022_PROGRAM_ID);
+      assert.equal(mintInfo.mintAuthority.toBase58(), mintAuthorityPda.toBase58())
 
-      const transaction = new Transaction().add(tx);
-      console.log("transaction", transaction);
-      const sig2 = await confirmTx(connection2, await connection2.sendTransaction(transaction, [tokenRecipient], { skipPreflight: false, preflightCommitment: "confirmed" }));
-      console.log("Your transaction signature", sig2);
-      // get the mint info to verify the mint was successful
-      let mintAccount = await getMint(
-         connection2,
+      // create token pool
+      const tokenPool = await createTokenPool(
+         connection,
+         payer,
          mint.publicKey,
          undefined,
          TOKEN_2022_PROGRAM_ID
       );
-      console.log(mintAccount);
-   });
+      console.log("Token pool created:", tokenPool);
+   })
 
+   it("mint compressed token to recipent", async () => {
+      // set up
+      const sig = await confirmTx(
+         connection,
+         await connection.requestAirdrop(tokenRecipient.publicKey, 10e9)
+      );
+      console.log("recipent Airdrop confirmed", sig);
+      console.log("recipent Airdrop done");
+      const transferAmount = 1e9;
+      const tokenRate = 1000;
+
+      // get required info from light protocol
+      // get token pool info
+      const tokenPoolInfos = await getTokenPoolInfos(connection, mint.publicKey);
+      console.log("token pool infos", tokenPoolInfos);
+      // get tree info
+      const stateTreeInfos = await connection.getStateTreeInfos();
+      console.log("state tree infos", stateTreeInfos);
+      // get token cpi auth pda
+      const registeredProgramPda = getRegisteredProgramPda()
+      console.log("registered program pda", registeredProgramPda.toBase58());
+      const [cpiAuthorityPda] = PublicKey.findProgramAddressSync(
+         [Buffer.from(CPI_AUTHORITY_SEED)],
+         CompressedTokenProgram.programId
+      );
+
+      console.log("cpi authority pda", cpiAuthorityPda.toBase58());
+      const accountCompressionAuthorityPda = getAccountCompressionAuthority()
+      console.log("account compression authority pda", accountCompressionAuthorityPda.toBase58());
+      // build the instruction
+      const ix = await program.methods.mintCompressedTokens(new anchor.BN(transferAmount), new anchor.BN(tokenRate))
+         .accounts({
+            mint: mint.publicKey,
+            tokenPoolPda: tokenPoolInfos[0].tokenPoolPda,
+            compressedTokenCpiAuthorityPda: cpiAuthorityPda,
+            accountCompressionAuthorityPda: accountCompressionAuthorityPda,
+            registeredProgramPda: registeredProgramPda,
+            noopProgram: noopProgram,
+            mintCreator: payer.publicKey,
+            mintAuthority: mintAuthorityPda,
+            stateTree: stateTreeInfos[0].tree,
+            recipient: tokenRecipient.publicKey,
+            tokenProgram: TOKEN_2022_PROGRAM_ID,
+            systemProgram: anchor.web3.SystemProgram.programId,
+            lightSystemProgram: LightSystemProgram.programId,
+            accountCompressionProgram: accountCompressionProgram,
+            compressedTokenProgram: CompressedTokenProgram.programId,
+            solPoolPda: null,
+         }).instruction();
+      const transaction = new Transaction().add(ix);
+      console.log("transaction", transaction);
+      const sig2 = await confirmTx(connection, await connection.sendTransaction(transaction, [tokenRecipient], { skipPreflight: false, preflightCommitment: "confirmed" }));
+      console.log("Minted compressed token transaction signature", sig2);
+
+      // get the mint info to verify the mint was successful
+      let mintAccount = await getMint(
+         connection,
+         mint.publicKey,
+         undefined,
+         TOKEN_2022_PROGRAM_ID
+      );
+      console.log("Mint info after mint", mintAccount);
+   })
 })
